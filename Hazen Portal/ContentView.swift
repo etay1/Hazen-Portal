3b64932e46dfaf795faea45648fa42dbbfb1a785
Elijah Tay <etay1@brockport.edu>
2022-09-20 01:11:45 -0400

    Initial Commit

---
+++ Hazen Portal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hazen Portal
+//
+//  Created by Elijah Tay on 9/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hazen Portal/Hazen_PortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hazen_PortalApp.swift
+//  Hazen Portal
+//
+//  Created by Elijah Tay on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Hazen_PortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
