1517a01106faabba968e06e4058fec21f3afa863
etay1 <etay1@brockport.edu>
2022-09-30 22:10:05 -0400

    asset import

--- Hazen Portal/Screen Views/HomeScreen.swift
+++ Hazen Portal/Screen Views/HomeScreen.swift
@@ -16,5 +16,59 @@ struct HomeScreen: View {
         ZStack{
             Text("Hello World")
         }
+        
+        /*
+         So I'm thinking we he have a VStack here
+            This will make all elements in here align vertically
+            I cant test it now but when I get to the library I will
+         
+         Either a single v stack that has the elements wrap after every 2 icons
+         VStack {
+             Button (with an icon parameter)
+             Button (with an icon parameter)
+             wraps
+             Button (with an icon parameter)
+             Button (with an icon parameter)
+             wraps
+             Button (with an icon parameter)
+             Button (with an icon parameter)
+             wraps
+             Button (with an icon parameter)
+             Button (with an icon parameter)
+         }
+         
+         ORRRR
+         4 VStacks like this
+         
+         VStack{
+            Button (with an icon parameter)
+            Button (with an icon parameter)
+         
+         }
+         VStack{
+            Button (with an icon parameter)
+            Button (with an icon parameter)
+         }
+         VStack{
+            Button (with an icon parameter)
+            Button (with an icon parameter)
+         }
+         VStack{
+            Button (with an icon parameter)
+            Button (with an icon parameter)
+         }
+         
+         
+         Then each button will just have a non authenticated destination to each page in the method body
+         
+         AppointmentScreen()
+         MessagesScreen()
+         etc...
+         
+         The issue here is that I dont think we can use an icon as a parameter it might force us to use a string
+         so if thats the case we can try to create a frame instead and somehow have the frame navigate as a button
+         but ill trial and error it tomorrow and try a few methods when i can preview everything
+         
+         */
     }
 }
