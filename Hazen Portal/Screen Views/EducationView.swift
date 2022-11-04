3f00260aebe7f05bf894b1a6c35895d36d8e9f68
etay1 <etay1@brockport.edu>
2022-11-03 21:03:58 -0400

    blah

--- Hazen Portal/Screen Views/EducationView.swift
+++ Hazen Portal/Screen Views/EducationView.swift
@@ -16,76 +16,76 @@ struct EducationView: View {
             
             ScrollView { //S
                 
-                //
-
+              
+                
                 
                 VStack (alignment: .leading){ //V1
                     Group { //G1
                         Text("Welcome to My-Hazen your patient portal for the Counseling, Health and Sports Medicine Clinic")
                             .foregroundColor(.accentColor)
-                            //.border(.red)
+                        //.border(.red)
                         
                         Text("CRISIS TEXT LINE --- NEED SOMEONE TO TALK TO? Text \"Got5U\" to 741-741-Free, 24/7, Confidential text support.")
-                           // .border(.red)
+                        // .border(.red)
                         
                         Text("If you have a non-urgent medical or psychiatric concern, call 585-395-2414 and press option 1 for the Nurse Advice line. These nurses do NOT have access to you records and are unable to provice prescription refills, test results or make appointments.")
-                           // .border(.red)
+                        // .border(.red)
                         
                         Text("For emergencies during this time, please call 911 or University Police at 585-395-2222")
-                           // .border(.red)
+                        // .border(.red)
                     } //G1
                     .multilineTextAlignment(.center)
                     .padding([.bottom], 5)
                     
-
+                    
                 }//V1
-
+                
                 .padding([.leading], 15)
-             
+                
                 .font(.headerText)
                 .foregroundColor(.red)
                 //.border(.red)
                 
                 Divider()
-
-        
+                
+                
                 VStack (alignment:.leading){ //V2
-                
-                LinkBox(text: "Active Listening", link:"https://brockport.medicatconnect.com/form/active-listening.pdf" )
-        
-                TextBox(text: "Treating listening as an active process, rather than a passive one. This means participating in conversation, rather than acting as an audience. Active listeners show they are listening, encourage sharing, and strive to understand the speaker")
-                        
-                //-----------
-                
-                LinkBox(text: "Anger Coping Skills", link:"https://brockport.medicatconnect.com/form/coping-skills-anger.pdf" )
                     
-                TextBox(text: "Anger triggers are the things that set you off. Knowing your triggers, and being cautious around them, will reduce the likelihood of your anger getting out of control.")
+                    LinkBox(text: "Active Listening", link:"https://brockport.medicatconnect.com/form/active-listening.pdf" )
                     
-                
-                //-----------
-                
-                LinkBox(text: "Anxiety", link:"https://brockport.medicatconnect.com/form/what-is-anxiety.pdf" )
+                    TextBox(text: "Treating listening as an active process, rather than a passive one. This means participating in conversation, rather than acting as an audience. Active listeners show they are listening, encourage sharing, and strive to understand the speaker")
                     
-                TextBox(text: "What is anxiety? How is it treated?")
+                    //-----------
                     
-                 //-----------
-                
-                LinkBox(text: "Tips for setting healthy boundary", link:"https://brockport.medicatconnect.com/form/healthy-boundaries-tips.pdf")
+                    LinkBox(text: "Anger Coping Skills", link:"https://brockport.medicatconnect.com/form/coping-skills-anger.pdf" )
                     
-                TextBox(text: "Personal boundaries are the limits and rules we set for ourselves within relationships.")
+                    TextBox(text: "Anger triggers are the things that set you off. Knowing your triggers, and being cautious around them, will reduce the likelihood of your anger getting out of control.")
                     
-                //-----------
-                
-                LinkBox(text: "Tips for setting boundaries with others", link:"https://brockport.medicatconnect.com/form/setting-boundaries.pdf" )
-                
-                TextBox(text: "Boundaries should be based on your values, or the things that are important to you.")
-        
-            } //V2
+                    
+                    //-----------
+                    
+                    LinkBox(text: "Anxiety", link:"https://brockport.medicatconnect.com/form/what-is-anxiety.pdf" )
+                    
+                    TextBox(text: "What is anxiety? How is it treated?")
+                    
+                    //-----------
+                    
+                    LinkBox(text: "Tips for setting healthy boundary", link:"https://brockport.medicatconnect.com/form/healthy-boundaries-tips.pdf")
+                    
+                    TextBox(text: "Personal boundaries are the limits and rules we set for ourselves within relationships.")
+                    
+                    //-----------
+                    
+                    LinkBox(text: "Tips for setting boundaries with others", link:"https://brockport.medicatconnect.com/form/setting-boundaries.pdf" )
+                    
+                    TextBox(text: "Boundaries should be based on your values, or the things that are important to you.")
+                    
+                } //V2
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding([.leading], 15)
                 //.border(.red)
                 
-              
+                
                 
                 VStack (alignment: .leading){ //V3
                     
@@ -112,11 +112,11 @@ struct EducationView: View {
                     TextBox(text: "Depression saps a person’s energy to do just about anything—even activities they enjoy. As a result, people with depression tend to become less active, which causes the depression to worsen. However, even a little bit of activity can help stop this cycle")
                     
                     //-----------
-                  
-                        LinkBox(text: "Emotions", link:"https://brockport.medicatconnect.com/form/basic-emotions-printout.pdf" )
-                        
-                        TextBox(text: "Emotions can be hard to describe at times. Here is a list of some basic emotions we experience.")
-           
+                    
+                    LinkBox(text: "Emotions", link:"https://brockport.medicatconnect.com/form/basic-emotions-printout.pdf" )
+                    
+                    TextBox(text: "Emotions can be hard to describe at times. Here is a list of some basic emotions we experience.")
+                    
                 } //V3
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding([.leading], 15)
@@ -128,10 +128,10 @@ struct EducationView: View {
                     TextBox(text: "Research has shown us that people who exercise regularly tend to be more resistant to many mental health issues. Exercise can help treat current symptoms and prevent future episodes.")
                     
                     //-----------
-                  
-                        LinkBox(text: "Facts about Grief", link:"https://brockport.medicatconnect.com/form/grief-fact-sheet.pdf" )
-                        
-                        TextBox(text: "The process of grieving often involves sadness, anger, loneliness,and other painful emotions.")
+                    
+                    LinkBox(text: "Facts about Grief", link:"https://brockport.medicatconnect.com/form/grief-fact-sheet.pdf" )
+                    
+                    TextBox(text: "The process of grieving often involves sadness, anger, loneliness,and other painful emotions.")
                     
                     //-----------
                     
@@ -151,7 +151,7 @@ struct EducationView: View {
                     
                     TextBox(text: "During the leaves on a stream exercise, you will visualize yourself resting near a stream. Whenever thoughts enter your mind, you will imagine placing them atop a leaf, and watching as they float away. Instead of seeing the world from “inside” your thoughts, you will take a step back and view them from afar. By doing so, you will gain perspective on your thoughts and feelings, reducing their power.")
                     
-              
+                    
                 }//V4
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding([.leading], 15)
@@ -189,52 +189,61 @@ struct EducationView: View {
                     
                     TextBox(text: "Self-care means taking time to do things")
                     
-    
+                    
                     
                 }//V5
                 .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading], 15)
-                    //.border(.red)
+                .padding([.leading], 15)
+                //.border(.red)
                 
                 VStack (alignment: .leading){ //V6
                     
                     //-----------
-                             
-                                      LinkBox(text: "Sleep Hygiene", link:"https://brockport.medicatconnect.com/form/sleep-hygiene-handout.pdf" )
-                                      
-                                      TextBox(text: "Tips for better sleep.")
-                                      
-                                      
-                                      //-----------
-                                      
-                                      LinkBox(text: "Stress Management Tips", link:"https://brockport.medicatconnect.com/form/stress-management-tips.pdf" )
-                                      
-                                      TextBox(text: "Tips to better manage your stress.")
-                                      
-                                      //-----------
-                                      
-                                      LinkBox(text: "Study Tips", link:"https://brockport.medicatconnect.com/form/study-tips.pdf" )
-                                      
-                                      TextBox(text: "Study Tips for ADHD and Anxiety.")
-                                      
-                                      //-----------
-                                      
-                                      LinkBox(text: "Time Management Tips", link:  "https://brockport.medicatconnect.com/form/time-management-tips.pdf" )
-                                      
-                                     TextBox(text: "Tips to better manage your time.")
+                    
+                    LinkBox(text: "Sleep Hygiene", link:"https://brockport.medicatconnect.com/form/sleep-hygiene-handout.pdf" )
+                    
+                    TextBox(text: "Tips for better sleep.")
+                    
+                    
+                    //-----------
+                    
+                    LinkBox(text: "Stress Management Tips", link:"https://brockport.medicatconnect.com/form/stress-management-tips.pdf" )
+                    
+                    TextBox(text: "Tips to better manage your stress.")
+                    
+                    //-----------
+                    
+                    LinkBox(text: "Study Tips", link:"https://brockport.medicatconnect.com/form/study-tips.pdf" )
+                    
+                    TextBox(text: "Study Tips for ADHD and Anxiety.")
+                    
+                    //-----------
+                    
+                    LinkBox(text: "Time Management Tips", link:  "https://brockport.medicatconnect.com/form/time-management-tips.pdf" )
+                    
+                    TextBox(text: "Tips to better manage your time.")
                     
                 }//V6
                 .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding([.leading], 15)
-                    //.border(.red)
-                    
-                
+                .padding([.leading], 15)
+                //.border(.red)
+         
                
-                
-                
-                
+          
                 
         }//S
+            .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .center)
+                //.border(.red)
+                .padding([.bottom], 15)
+                .disabled(menuShowing ? true: false)
+                .blur(radius: menuShowing ? 5: 0)
+            
+            if(menuShowing) {
+                MenuView(menuShowing: $menuShowing, size: 40)
+                    .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .center)
+                //  .border(.red)
+                     
+            }
              
             
         }//V0
